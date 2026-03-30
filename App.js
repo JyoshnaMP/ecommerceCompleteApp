50e266534caeb98951e8d9ad6e9c9be7e4a2ed3e
Jyoshna <jyoshna.d.n@gmail.com>
2026-03-31 05:23:03 +0530

    commit

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './navigation/stackNavigator';
-import { store, persistor } from './store'; // ✅ import persistor too
+import { store, persistor } from './store';
 import {UserContext} from './userContext';
 import {Provider} from "react-redux";
 import {ModalPortal} from "react-native-modals";
-import { PersistGate } from 'redux-persist/integration/react'; // ✅ new import
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>  {/* ✅ wraps everything */}
+        <PersistGate loading={null} persistor={persistor}>  
           <UserContext>
             <StackNavigator/>
             <ModalPortal/>
--- store.js
+++ store.js
@@ -4,26 +4,26 @@ import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { combineReducers } from '@reduxjs/toolkit';
 
-// Config - save to phone storage
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
 };
 
-// Combine reducers
+
 const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
-// Wrap with persist
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Create store
+
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // needed for redux-persist
+      serializableCheck: false, 
     }),
 });
 
